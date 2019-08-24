b156ebd57d3d61d08e5a441e95ba633328704b1e
pratikstemkar <pratiktemkarofficial@gmail.com>
2019-08-24 12:23:29 +0530

    Hello World Commit

---
+++ todo-app/src/App.js
@@ -0,0 +1,12 @@
+// Import React dependencies
+import React from 'react'
+
+// App function to add other components
+function App() {
+    return (
+        <h1>Hello World!!</h1>
+    )
+}
+
+// Export App to index.js
+export default App
---
+++ todo-app/src/index.js
@@ -0,0 +1,9 @@
+// Import React Dependencies
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// Import App from src/App.js
+import App from './App'
+
+// Render the app in index.html
+ReactDOM.render(<App />, document.getElementById('root'))
